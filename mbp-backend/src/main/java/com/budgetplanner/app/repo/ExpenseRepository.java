a2e973f95559f336b6af38254176fcb65a8e71aa
Sunil-Reddy-Gummalla <gummalla.sunilreddy@gmail.com>
2024-05-18 21:37:15 +0530

    project setup-2

--- mbp-backend/src/main/java/com/budgetplanner/app/repo/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.budgetplanner.app.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.budgetplanner.app.entity.Expense;
-
-public interface ExpenseRepository extends MongoRepository<Expense, String> {
-}
